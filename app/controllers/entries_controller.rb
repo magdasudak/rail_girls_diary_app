0510916dddbb58d1d4b3e0334ade24b2874e2cd6
cmagdalenaa@gmail.com <themarios@gmail.com>
2016-09-18 10:34:28 +0200

    initial

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,33 @@
+class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all
+  end
+
+  def show
+    @entry = Entry.find(params["id"])
+  end
+
+  def create 
+  	entry_params = params["entry"].permit("title", "contents")
+    entry = Entry.create(entry_params)
+    redirect_to(entry_path(entry), notice: "New entry created")
+  end
+  def edit
+    @entry = Entry.find(params["id"]) 
+  end
+
+  def update
+  entry_params = params["entry"].permit("title", "contents")
+  entry = Entry.find(params["id"])
+  entry.update(entry_params)
+  redirect_to(entry_path(entry), notice:"Entry updated")
+  end
+
+  def destroy
+  @entry = Entry.find(params["id"])
+  @entry.destroy
+  redirect_to(entries_path, notice: "Entry deleted") 
+  	
+  end
+
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+  def index
+  
+  @entries = Entry.all
+  end
+end
